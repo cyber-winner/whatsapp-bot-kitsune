541e618db5d31aac7ed61e6c6947c766b74be36a
cyber-winner <sarkarsuraj32008@gmail.com>
2026-08-03 22:28:53 +0530

    chore: delete test files

--- test_contacts.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-if (fs.existsSync('./store-data-for-use/contacts.json')) {
-  const data = fs.readFileSync('./store-data-for-use/contacts.json', 'utf8');
-  const contacts = JSON.parse(data);
-  console.log(`Found ${Object.keys(contacts).length} contacts.`);
-} else {
-  console.log('No contacts.json found.');
-}
